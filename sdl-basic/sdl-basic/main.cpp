fa8e96a6bf01610aa53728c2919b09d3107618de
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2017-05-29 00:11:35 -0300

    chi: create a blank window

--- sdl-basic/sdl-basic/main.cpp
+++ sdl-basic/sdl-basic/main.cpp
@@ -11,14 +11,44 @@
 
 int main(int argc, const char * argv[]) {
 
-    if (SDL_Init(SDL_INIT_VIDEO) > 0) {
-        std::cout << "SDL init failed." << std::endl;
-        return 1;
-    }
-    
-    std::cout << "SDL Init succeeded." << std::endl;
-    
+  const int SCREEN_WIDTH = 800;
+  const int SCREEN_HEIGHT = 600;
+  
+  if (SDL_Init(SDL_INIT_VIDEO) > 0) {
+    std::cout << "SDL init failed." << std::endl;
+    return 1;
+  }
+  
+  SDL_Window *window = SDL_CreateWindow(
+    "Particle Fire Explosion",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    SDL_WINDOW_SHOWN
+  );
+  
+  if (window == NULL) {
+    std::cout << "Could not create window: " << SDL_GetError() << std::endl;
     SDL_Quit();
-    
-    return 0;
+    return 2;
+  }
+  
+  bool quit = false;
+  SDL_Event event;
+  
+  while(!quit) {
+    //  Update particles
+    //  Draw particles
+    //  Check for messages/events
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+  }
+  
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
