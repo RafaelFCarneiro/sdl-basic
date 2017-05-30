9f69593314de3f8afd2bdf4ab83a72070169820e
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2017-05-29 22:45:44 -0300

    Rendering and setting white

--- sdl-basic/sdl-basic/main.cpp
+++ sdl-basic/sdl-basic/main.cpp
@@ -19,20 +19,43 @@ int main(int argc, const char * argv[]) {
     return 1;
   }
   
-  SDL_Window *window = SDL_CreateWindow(
-    "Particle Fire Explosion",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    SCREEN_WIDTH,
-    SCREEN_HEIGHT,
-    SDL_WINDOW_SHOWN
-  );
+  SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   
   if (window == NULL) {
     std::cout << "Could not create window: " << SDL_GetError() << std::endl;
     SDL_Quit();
     return 2;
   }
+  
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888,
+    SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT);
+  
+  if (renderer == NULL) {
+    std::cout << "Could not create render: " << SDL_GetError() << std::endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 3;
+  }
+  
+  if (texture == NULL) {
+    std::cout << "Could not create texture: " << SDL_GetError() << std::endl;
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    return 4;
+  }
+  
+  Uint32 *buffer = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+  
+  // setting white - 0xFF is 255
+  memset(buffer, 0xFF, SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(Uint32));
+  
+  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH*sizeof(Uint32));
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
   
   bool quit = false;
   SDL_Event event;
@@ -48,6 +71,9 @@ int main(int argc, const char * argv[]) {
     }
   }
   
+  delete []buffer;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyTexture(texture);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
